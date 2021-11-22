e1d9ac4728a936c51e9d05054da76ff79de41086
leenarane93 <leenarane93@outlook.com>
2021-11-22 11:06:53 +0530

    jjj

--- examples/transition.js
+++ examples/transition.js
@@ -1,83 +1,85 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
-var playlistSavePath = "C:\\Images\\Playlist\\";
+var playlistSavePath = "C:\\Images\\Playlists\\";
 var plname = "";
 var plduration = 30;
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   console.log(req);
-  var videoshow = require('../')
+  var videoshow = require("../");
 
-var audio = __dirname + '/../test/fixtures/song.mp3'
+  var audio = __dirname + "/../test/fixtures/song.mp3";
 
-var options = {
-  transition: true
-}
+  var options = {
+    transition: true,
+  };
 
-var images = files;
+  var images = files;
 
-videoshow(images, options)
-  .audio()
-  .save(playlistSavePath+'ImageVideo.mp4')
-  .on('start', function (command) {
-    console.log('ffmpeg process started:', command)
-  })
-  .on('error', function (err) {
-    console.error('Error:', err)
-  })
-  .on('end', function (output) {
-    console.log('Video created in:', output)
-  })
-    res.send('<h1>Action Completed</h1>');
+  videoshow(images, options)
+    .audio()
+    .save(playlistSavePath + "ImageVideo.mp4")
+    .on("start", function (command) {
+      console.log("ffmpeg process started:", command);
+    })
+    .on("error", function (err) {
+      console.error("Error:", err);
+    })
+    .on("end", function (output) {
+      console.log("Video created in:", output);
+    });
+  res.send("<h1>Action Completed</h1>");
 
+  //Videos Merge
+  ("use strict");
 
-    //Videos Merge
-    'use strict';
+  let videoStitch = require("video-stitch");
 
-let videoStitch = require('video-stitch');
+  let videoMerge = videoStitch.merge;
 
-let videoMerge = videoStitch.merge;
-
-videoMerge()
-  .original({
-    "fileName": playlistSavePath + plname + ".mp4",
-    "duration": plduration
-  })
-  .clips([
-    {
-      "startTime": "00:00:00",
-      "fileName":  __dirname + '/../test/fixtures/video.mp4',
-      "duration": "00:00:15"
-    },
-    {
-      "startTime": "00:00:00",
-      "fileName":  __dirname + '/../test/fixtures/AVI_Media.avi',
-      "duration": "00:00:15"
-    }
-  ])
-  .merge()
-  .then((outputFile) => {
-    console.log('path to output file', outputFile);
-  },(error)=>{
-    console.log(error);
-  });
-
+  videoMerge()
+    .original({
+      fileName: playlistSavePath + plname + ".mp4",
+      duration: plduration,
+    })
+    .clips([
+      {
+        startTime: "00:00:00",
+        fileName: __dirname + "/../test/fixtures/video.mp4",
+        duration: "00:00:15",
+      },
+      {
+        startTime: "00:00:00",
+        fileName: __dirname + "/../test/fixtures/AVI_Media.avi",
+        duration: "00:00:15",
+      },
+    ])
+    .merge()
+    .then(
+      (outputFile) => {
+        console.log("path to output file", outputFile);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
 });
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
 
-  files =[{
-    path: __dirname + '/../test/fixtures/step_1.png',
+files = [
+  {
+    path: __dirname + "/../test/fixtures/step_1.png",
     disableFadeOut: true,
-    loop: 2
-  },{
-    path: __dirname + '/../test/fixtures/step_2.png',
+    loop: 2,
+  },
+  {
+    path: __dirname + "/../test/fixtures/step_2.png",
     disableFadeIn: true,
-    loop: 2
-  }
-]
-
+    loop: 2,
+  },
+];
--- examples/videomerge.js
+++ examples/videomerge.js
@@ -15,11 +15,18 @@ var outputFilePath = Date.now() + "output.mp4";
 const bodyParser = require("body-parser");
 
 const multer = require("multer");
+const cors = require("cors");
 
 const app = express();
-var playlistSavePath = "C:\\Images\\Playlist\\";
+var playlistSavePath = "C:\\Images\\Playlists\\";
 var dir = "public";
 var subDirectory = "public/uploads";
+const ffmpegInstaller = require("@ffmpeg-installer/ffmpeg");
+const ffprobe = require("@ffprobe-installer/ffprobe");
+
+const ffmpeg = require("fluent-ffmpeg")()
+  .setFfprobePath(ffprobe.path)
+  .setFfmpegPath(ffmpegInstaller.path);
 
 if (!fs.existsSync(dir)) {
   fs.mkdirSync(dir);
@@ -50,36 +57,85 @@ const videoFilter = function (req, file, cb) {
 
 var upload = multer({ storage: storage, fileFilter: videoFilter });
 
+const imageFilter = function (req, file, cb) {
+  // Accept videos only
+  if (!file.originalname.match(/\.(png)$/)) {
+    req.fileValidationError = "Only video files are allowed!";
+    return cb(new Error("Only video files are allowed!"), false);
+  }
+  cb(null, true);
+};
+
+var imgupload = multer({ storage: storage, fileFilter: imageFilter });
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
+app.use(cors());
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+// });
 
 const PORT = process.env.PORT || 3000;
-
 app.get("/", (req, res) => {
   res.send("Application is in running mode.");
 });
 
-app.post("/imageToVideo", upload.array("imgfiles", 1000), (req, res) => {
-  list = "";
-  if (req.imgfiles) {
-    req.imgfiles.forEach((file) => {
-      list = `file ${file.filename}`;
-      var idx = list.lastIndexOf(".");
-      list = list.substring(0, idx);
-      var videoshow = require("../");
+// app.post("/imageToVideo", imgupload.array("imgfiles", 1000), (req, res) => {
+//   list = "";
+//   if (req.imgfiles) {
+//     req.imgfiles.forEach((file) => {
+//       list = `file ${file.filename}`;
+//       var idx = list.lastIndexOf(".");
+//       list = list.substring(0, idx);
+//       var videoshow = require("../");
 
-      var audio = __dirname + "/../test/fixtures/song.mp3";
+//       var audio = __dirname + "/../test/fixtures/song.mp3";
 
-      var options = {
-        transition: true,
-      };
+//       var options = {
+//         transition: true,
+//       };
 
-      var images = file;
+//       var images = file;
 
+//       videoshow(images, options)
+//         .audio()
+//         .save("img.mp4")
+//         .on("start", function (command) {
+//           console.log("ffmpeg process started:", command);
+//         })
+//         .on("error", function (err) {
+//           console.error("Error:", err);
+//         })
+//         .on("end", function (output) {
+//           console.log("Video created in:", output);
+//         });
+//       res.send("<h1>Action Completed</h1>");
+//     });
+//   }
+// });
+
+app.post("/imageToVideo", (req, res) => {
+  console.log(req.body);
+  var videoshow = require("../");
+
+  var audio = __dirname + "/../test/fixtures/song.mp3";
+
+  var options = {
+    transition: true,
+  };
+
+  var images = req.body;
+  var plName = req.body[0].plid;
+  req.body.forEach((ele) => {
+    if (ele.type == "image") {
       videoshow(images, options)
         .audio()
-        .save(playlistSavePath + list + ".mp4")
+        .save(playlistSavePath + plName + "\\" + "ImageVideo.mp4")
         .on("start", function (command) {
           console.log("ffmpeg process started:", command);
         })
@@ -89,10 +145,56 @@ app.post("/imageToVideo", upload.array("imgfiles", 1000), (req, res) => {
         .on("end", function (output) {
           console.log("Video created in:", output);
         });
-      res.send("<h1>Action Completed</h1>");
-    });
-  }
+    } else if (ele.type == "gif") {
+      //gifConvert(ele.path, playlistSavePath + plName, ele.fname);
+    }
+    res.send("<h1>Action Completed</h1>");
+  });
 });
+app.get("/", (req, res) => {
+  console.log(req);
+  var videoshow = require("../");
+
+  var audio = __dirname + "/../test/fixtures/song.mp3";
+
+  var options = {
+    transition: true,
+  };
+
+  var images = files;
+
+  videoshow(images, options)
+    .audio()
+    .save(playlistSavePath + "ImageVideo.mp4")
+    .on("start", function (command) {
+      console.log("ffmpeg process started:", command);
+    })
+    .on("error", function (err) {
+      console.error("Error:", err);
+    })
+    .on("end", function (output) {
+      console.log("Video created in:", output);
+    });
+  res.send("<h1>Action Completed</h1>");
+});
+
+function gifConvert(path, destPath, fname, duration) {
+  ffmpeg
+    .input(path)
+    .outputOptions([
+      "-pix_fmt yuv420p",
+      "-c:v libx264",
+      "-movflags +faststart",
+      "-filter:v crop='floor(in_w/2)*2:floor(in_h/2)*2'",
+    ])
+    .noAudio()
+    .output(destPath + "GIF.mp4")
+    .on("end", () => {
+      console.log("Ended");
+    })
+    .on("error", (e) => console.log(e))
+    .run();
+}
 
 app.post("/videoMerge", upload.array("files", 1000), (req, res) => {
   list = "";
@@ -136,3 +238,16 @@ app.post("/videoMerge", upload.array("files", 1000), (req, res) => {
 app.listen(PORT, () => {
   console.log(`App is listening on Port ${PORT}`);
 });
+
+files = [
+  {
+    path: __dirname + "/../test/fixtures/step_1.png",
+    disableFadeOut: true,
+    loop: 2,
+  },
+  {
+    path: __dirname + "/../test/fixtures/step_2.png",
+    disableFadeIn: true,
+    loop: 2,
+  },
+];
